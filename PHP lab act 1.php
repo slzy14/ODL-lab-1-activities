fc36d406c9ea67cd5ced0a2063a436ab982879a6
M307users <m307user@m307.uc-bcf.edu.ph>
2022-09-15 07:18:27 +0400

    initial upload

---
+++ PHP lab act 1.php
@@ -0,0 +1,12 @@
+
+<?php
+echo "Twinkle, Twinkle little star .\n";
+
+
+$twinkle = "Hello" ;
+
+$star = "World";
+
+echo $twinkle.", ".$twinkle." little ". $star;
+
+?>
---
+++ PHP lab act 3.php
@@ -0,0 +1,21 @@
+<?php
+$varia = 8;
+
+
+echo "Value is now ". $varia. ".\r\n";
+
+echo "Add 2. Value is now " . ($varia+=2) .".\r\n";
+
+echo "Subtract 4. Value is now ".($varia-=4).".\r\n";
+
+echo "Multiply by 5. Value is now " .($varia*=5).".\r\n";
+
+echo "Divide by 3. Value is now " .($varia/=3).".\r\n";
+
+echo "Increment value by one. Value is now " . ($varia+=1).".\r\n";
+
+echo "Decrement value by one. Value is now " . ($varia-=1).".\r\n";
+
+
+
+?>
